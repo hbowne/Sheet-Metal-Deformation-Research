21de61bfc7262ac316ea11d83e40615dc8733254
Maimaruvit <maimaruvit5@gmail.com>
2025-03-07 20:22:26 -0500

    Matlab files for processing data

---
+++ Sheet_Metal_Robotics_molly/2-25+2-28/dataproc_2_9_A1.m
@@ -0,0 +1,32 @@
+data = load("2_9_A1");
+
+%Truncate data based off of initial plot, adjust values to delete flags
+x_length = data(1,85:646);
+z_depth = data(2,85:646);
+
+%Trendline
+z_linear = z_depth(456:562);
+x_linear = x_length(456:562);
+coeff = polyfit(x_linear, z_linear, 1);
+lin_fit = coeff(1)*x_length + coeff(2);
+
+%Adjust Data for Surface Tilt
+distance = z_depth - lin_fit;
+
+%Filter data
+filtered_z_depth = smoothdata(distance, "movmean", 35);
+
+%Original Data Plot
+%plot(x_length, z_depth)
+%figure;
+
+%Local Maxima
+max = islocalmax(filtered_z_depth);
+
+plot(x_length, distance);
+hold on;
+plot(x_length, filtered_z_depth, x_length(max),filtered_z_depth(max), 'r*');
+title("2/9 A1 Depth vs Length");
+xlabel("Length (mm)");
+ylabel("Depth (mm)");
+legend("Original Data", "Filtered Data", "Max Depth")
