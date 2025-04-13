27325aae62fa759f7da0dfa6b6f7a2f212183a4d
hbowne <bownef@rpi.edu>
2025-04-13 15:39:37 -0400

    data process for 4_13_30A and 4_13_50A

---
+++ Sheet_Metal_Robotics_molly/4-13-Stiffness-Scans/dataproc_4_13_30A_3cm.m
@@ -0,0 +1,37 @@
+figure;
+data = load("4_13_30A_3cm");
+dataAve = load("4_13_base");
+
+coeff = polyfit(dataAve(1, 1:1200), dataAve(2, 1:1200), 1);
+%plot(dataAve(1, 1:1200), dataAve(2, 1:1200))
+
+%Truncate data based off of initial plot, adjust values to delete flags
+x_length = data(1,490:1274);
+z_depth = data(2,490:1274);
+
+%Original Data Plot
+%plot(x_length, z_depth)
+%figure;
+
+%Trendline
+z_linear = z_depth;
+x_linear = x_length;
+%coeff = polyfit(x_linear, z_linear, 1);
+lin_fit = coeff(1)*x_length + coeff(2);
+
+%Adjust Data for Surface Tilt
+distance = z_depth - lin_fit;
+
+%Filter data
+filtered_z_depth = smoothdata(distance, "movmean", 15);
+
+%Local Maxima
+max = islocalmax(filtered_z_depth);
+
+plot(x_length, distance);
+hold on;
+plot(x_length, filtered_z_depth, x_length(max),filtered_z_depth(max), 'r*');
+title("4/13 30A 3cm Depth vs Length");
+xlabel("Length (mm)");
+ylabel("Depth (mm)");
+legend("Original Data", "Filtered Data", "Max Depth")
